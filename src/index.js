190297319950d7a06ecff644bd637d1daf2ca9d2
Shubham Yelekar <shubhamyelekar0@gmail.com>
2025-07-26 16:59:32 +0530

    cors origin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Backend working !");
+import { app } from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Server running OK on ${PORT}`);
+});
