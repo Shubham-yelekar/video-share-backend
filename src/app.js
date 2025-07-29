6a82b5fbd09372434c19491ddb5cf5abc20154fd
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-07-29 16:03:15 +0530

    created first api : healthcheck

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+import healthCheck from "./routes/healthCheck.routes.js";
+
+app.use("/api/v1/healthCheck", healthCheck);
+
 export { app };
