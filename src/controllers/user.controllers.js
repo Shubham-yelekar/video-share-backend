ec1f700e8a34e6ff13bbf5f89e660a78d66148b7
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-08-08 18:15:34 +0530

    login user api

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,81 +1,156 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-import {User } from "../models/users.models.js"
-import {uploadOnCloudinary, deleteFromCloudinary} from '../utils/cloudnary.js'
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/users.models.js";
+import {
+  uploadOnCloudinary,
+  deleteFromCloudinary,
+} from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async (req , res) =>{
-  const {fullname, email, username, password} = req.body 
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
 
-  if([fullname,email, username, password].some((field)=> field?.trim() === "")){
-      throw new ApiError(400, "fullname is required")
-    }
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
 
-  const existedUser = await User.findOne({$or:[{username}, {email}]})
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullname, email, username, password } = req.body;
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "fullname is required");
+  }
+
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
   if (existedUser) {
-    throw new ApiError(409, "user with email.username already exist")
+    throw new ApiError(409, "user with email.username already exist");
   }
   console.warn(req.files);
-  
-  const avatarLocalPath = req.files?.avatar?.[0]?.path
-  let avatar ;
+
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  let avatar;
   try {
-    avatar = await uploadOnCloudinary(avatarLocalPath)
+    avatar = await uploadOnCloudinary(avatarLocalPath);
     console.log("🚀 Avatar Uploaded", avatar);
-    
-  } catch (error){
+  } catch (error) {
     console.log("🔴 Failed to upload avatar", error);
-    
-    throw new ApiError(500, "🔴 Failed to upload avatar")
+
+    throw new ApiError(500, "🔴 Failed to upload avatar");
   }
 
-   let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
-    }
-    
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
- let coverImage 
+  let coverImage;
   try {
-    coverImage = await uploadOnCloudinary(coverImageLocalPath)
-    console.log("🚀 Cover image Uploaded", coverImage); 
-  } catch (error){
+    coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    console.log("🚀 Cover image Uploaded", coverImage);
+  } catch (error) {
     console.log("🔴 Failed to upload cover image", error);
-    throw new ApiError(500, "🔴 Failed to upload cover image")
+    throw new ApiError(500, "🔴 Failed to upload cover image");
   }
-
 
   try {
     const user = await User.create({
-      fullname, 
+      fullname,
       avatar: avatar.url,
       coverImage: coverImage?.url || "",
       email,
       password,
-      username: username.toLowerCase()
-  
-    })
-  
-    const createdUser = await User.findById(user._id).select("-password -refreshToken")
-  
+      username: username.toLowerCase(),
+    });
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
     if (!createdUser) {
-      throw new ApiError(500, "registering user error")
+      throw new ApiError(500, "registering user error");
     }
-  
-    return res.status(201).json(new ApiResponse(200, createdUser, "User registor succesfully "))
+
+    return res
+      .status(201)
+      .json(new ApiResponse(200, createdUser, "User registor succesfully "));
   } catch (error) {
     console.log("🔴 User creation failed");
-    if(avatar) {
-      await deleteFromCloudinary(avatar.public_id)
+    if (avatar) {
+      await deleteFromCloudinary(avatar.public_id);
     }
-    if(coverImage) {
-      await deleteFromCloudinary(coverImage.public_id)
+    if (coverImage) {
+      await deleteFromCloudinary(coverImage.public_id);
     }
 
-     throw new ApiError(500, "🔴 registering user error image were deleted")
+    throw new ApiError(500, "🔴 registering user error image were deleted");
   }
-})
+});
 
-export {
-  registerUser
-}
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!email || !username) {
+    throw new ApiError("400", "Missing fields");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError("404", "User does not exist");
+  }
+
+  const isPasswordCorrent = await user.isPasswordCorrent(password);
+  if (!isPasswordCorrent) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
